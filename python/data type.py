01e56c8c896d5d831ac632715d4b6549930b685d
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-03-01 16:28:18 +0200

    plp list assignment

---
+++ python/data type.py
@@ -0,0 +1,23 @@
+a=100
+print("the type of variable having the value",a, "is", type(a))
+print(a)
+b=True
+c=False
+d=30.45
+e=10+3j
+f=(('juma', 124, 19.4))
+g=(["juma", 1251, 18.45])
+print("the type of variable having the value",b, "is", type(b))
+print("the type of variable having the value",c, "is", type(c))
+print("the type of variable having the value",d, "is", type(d))
+print("the type of variable having the value",e, "is", type(e))
+print("the type of variable having the value",f, "is", type(f))
+print("the type of variable having the value",g, "is", type(g))
+print(a,b,c,d,e,f,g)
+str='hello REA innovation'
+print(str)
+print(str[0])
+print(str[2:5]) #its print the range of strings of 2:5
+print(str[6:]) # it print everything next from string 6
+print(str + "you are the most incredible tech company in Tanzania keep going on")
+print(str *4) #for print no. of the string you define (repetition)
---
+++ python/pythonlist.py
@@ -0,0 +1,8 @@
+list= ['SHARON', 1251, "REA", 3.14, 3+2j]
+tinylist=[1251, "Pamda"]
+print(list)
+print(tinylist)
+print(list[0])
+print(list[2:4])
+print(list + tinylist)
+print(tinylist * 4)
