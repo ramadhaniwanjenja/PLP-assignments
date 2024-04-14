6768834715d8c3cbd7b18ade91ca11e351c1801f
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-04-14 17:26:50 +0200

    dom and events in html

---
+++ web/java/hello.js
@@ -0,0 +1,79 @@
+console.log("HELLO REA Innovation")
+console.log("hello for now REA you have to manetise your service and prodeuct");
+console.log("shafii can you please work hard");
+
+var name=" shafii"
+console.log(name);
+
+let message="shafii is now on it trying to figure out how we can make it"
+console.log(message);
+const pi=3.1452;
+console.log(pi);
+// message Message MESSAGE
+//camelCase -firstName
+//snake_case -last_name
+var success=true;
+var fail=false; // boolen for javascript
+var int=100; // for interger incase for decimal number we use float
+console.log(int);
+var int2;
+var int4=50; 
+console.log(int2);
+var int3="100";// this is defined as string
+console.log(int3);
+console.log(int + int3+int4);
+//addtion operation
+console.log(2+4);
+// substraction 
+console.log(500-20);
+// multiplication
+console.log(60*60);
+//exponential
+console.log(4**5);
+//division
+console.log(1000/4);
+//reminder -%
+console.log(100%3);
+ var x=100;
+ var a=100;
+ var c=100;
+ var d=100;
+ var e=100;
+ var f=100;
+ var g=100;
+ var h=100;
+ var b=50;
+ //increment++
+// x++;
+ console.log(x);
+ // decrement
+ a--;
+ console.log(a);
+
+ c-=b;
+ d +=b;
+ f/=b;
+ g*=b;
+ h%=b;
+ console.log(c);
+ console.log(d);
+ console.log(e);
+ console.log(f);
+ console.log(g);
+ console.log(h);
+
+ //equal ==
+console.log(a==b);
+//nor equal !=
+console.log(a!=b);
+//greater than >
+console.log(a>b);
+//less than <
+console.log(a<b);
+//AND && same apply to NOT != and OR ||
+if(a==100 && b==50){
+    console.log(true);
+}
+else{console.log(false);}
+
+
---
+++ web/java/index.js
@@ -0,0 +1,29 @@
+//using var 
+//keyword variable_name = value;
+var name="shafii";
+console.log(name);
+// using let
+let job="electrical engineering";
+console.log(job);
+//using constant
+const pi=3.1452;
+console.log(pi);
+//age
+//name
+//location
+console.log('29');
+console.log("hello Mr shafii");
+console.log("tanzania, Africa");
+//nameing convenction in js
+//camelCase
+var firstName;
+var leanerFirstName;
+//snake_case
+var last_name;
+var leaner_last_name;
+
+var name_1;
+var name_2;
+//important  to note
+var name, Name, NAME;
+
